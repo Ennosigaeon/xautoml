996b641e827651b829dd1ae70730334cd7d6a11f
Marc Zoeller <m.zoeller@usu.de>
2021-05-12 19:28:50 +0200

    Basic react demo

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,29 @@ import {
 
 import { requestAPI } from './handler';
 
+import { MainAreaWidget } from '@jupyterlab/apputils';
+
+import { ILauncher } from '@jupyterlab/launcher';
+
+import { reactIcon } from '@jupyterlab/ui-components';
+
+import { CounterWidget } from './widget';
+
+/**
+ * The command IDs used by the react-widget plugin.
+ */
+namespace CommandIDs {
+  export const create = 'create-react-widget';
+}
+
 /**
  * Initialization data for the xautoml extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'xautoml:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  optional: [ILauncher],
+  activate: (app: JupyterFrontEnd, launcher: ILauncher) => {
     console.log('JupyterLab extension xautoml is activated!');
 
     requestAPI<any>('get_example')
@@ -23,6 +39,28 @@ const extension: JupyterFrontEndPlugin<void> = {
           `The xautoml server extension appears to be missing.\n${reason}`
         );
       });
+
+    const { commands } = app;
+
+    const command = CommandIDs.create;
+    commands.addCommand(command, {
+      caption: 'Create a new React Widget',
+      label: 'React Widget',
+      icon: args => (args['isPalette'] ? null : reactIcon),
+      execute: () => {
+        const content = new CounterWidget();
+        const widget = new MainAreaWidget<CounterWidget>({ content });
+        widget.title.label = 'React Widget';
+        widget.title.icon = reactIcon;
+        app.shell.add(widget, 'main');
+      }
+    });
+
+    if (launcher) {
+      launcher.add({
+        command
+      });
+    }
   }
 };
 
