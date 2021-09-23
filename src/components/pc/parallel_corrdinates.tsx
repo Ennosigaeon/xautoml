480974b4adb6018a9db693189a22a5fce45d0a56
Marc Zoeller <m.zoeller@usu.de>
2021-09-23 18:39:00 +0200

    Make PC responsive

--- src/components/pc/parallel_corrdinates.tsx
+++ src/components/pc/parallel_corrdinates.tsx
@@ -1,10 +1,11 @@
 import * as cpc from "./model";
-import {PCChoice} from "./pc_choice";
 import React from "react";
 import * as d3 from "d3";
-import {PCLine} from "./pc_line";
 import {Runhistory} from "../../model";
 import {ParCord} from "./util";
+import {PCChoice} from "./pc_choice";
+import {PCLine} from "./pc_line";
+import {LoadingIndicator} from "../loading";
 
 
 interface PCProps {
@@ -14,22 +15,44 @@ interface PCProps {
 interface PCState {
     model: cpc.Model
     highlightedLines: Set<string>
+    width: number
 }
 
 export class ParallelCoordinates extends React.Component<PCProps, PCState> {
 
+    private readonly HEIGHT = 65
     private readonly root: cpc.Choice;
+    private readonly container: React.RefObject<HTMLDivElement>;
 
     constructor(props: PCProps) {
         super(props)
-        this.state = {model: ParCord.parseRunhistory(this.props.runhistory), highlightedLines: new Set<string>()}
+        this.state = {
+            model: ParCord.parseRunhistory(this.props.runhistory),
+            highlightedLines: new Set<string>(),
+            width: undefined
+        }
 
         // init root node
         this.root = new cpc.Choice('', this.state.model.axes, false);
+        this.container = React.createRef<HTMLDivElement>()
 
         this.onCollapse = this.onCollapse.bind(this)
         this.onExpand = this.onExpand.bind(this)
         this.highlightLines = this.highlightLines.bind(this)
+    }
+
+    componentDidMount() {
+        if (this.container.current.clientWidth > 0)
+            this.setDimensions()
+        else
+            // Jupyter renders all components before output containers are rendered.
+            // Delay rendering to get the container width.
+            window.setTimeout(() => this.setDimensions(), 1000)
+    }
+
+    private setDimensions() {
+        const width = this.container.current.clientWidth
+        this.setState({width: width === 0 ? 1000 : width})
     }
 
     private onCollapse(choice: cpc.Choice) {
@@ -50,27 +73,37 @@ export class ParallelCoordinates extends React.Component<PCProps, PCState> {
     }
 
     public render() {
-        const width = 1000
-        const height = 500
+        const {model, highlightedLines, width} = this.state
+        if (!width) {
+            // Render loading indicator while waiting for delayed re-rendering with correct container width
+            return (
+                <div ref={this.container} style={{width: '100%'}}>
+                    <LoadingIndicator loading={true}/>
+                </div>
+            )
+        }
+
+        // Estimate height based on maximum number of components
+        const height = this.HEIGHT * Math.max(...this.root.axes.map(a => a.choices.length))
         const yScale = d3.scaleBand([this.root.label.toString()], [0, height / this.root.getHeightWeight()])
         this.root.layout([0, width], yScale)
 
-        const {model, highlightedLines} = this.state
-
         return (
-            <svg className={'pc'} width={`${width}px`} height={`${height}px`}>
-                <PCChoice choice={this.root} parent={undefined}
-                          onCollapse={this.onCollapse}
-                          onExpand={this.onExpand}
-                          onChoiceHover={this.highlightLines}/>
-                {this.state.model.lines.map(line => {
-                        return < PCLine key={line.id}
-                                        model={model}
-                                        line={line}
-                                        highlight={highlightedLines.has(line.id)}/>
-                    }
-                )}
-            </svg>
+            <div className={'pc-container'} style={{paddingBottom: `${(height / width) * 100}%`}}>
+                <svg className={'pc'} preserveAspectRatio={"xMinYMin meet"} viewBox={`0 0 ${width} ${height}`}>
+                    <PCChoice choice={this.root} parent={undefined}
+                              onCollapse={this.onCollapse}
+                              onExpand={this.onExpand}
+                              onChoiceHover={this.highlightLines}/>
+                    {this.state.model.lines.map(line => {
+                            return <PCLine key={line.id}
+                                           model={model}
+                                           line={line}
+                                           highlight={highlightedLines.has(line.id)}/>
+                        }
+                    )}
+                </svg>
+            </div>
         )
     }
 }
