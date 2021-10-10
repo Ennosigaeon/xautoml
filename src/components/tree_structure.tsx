12346798b9ae26ff415d5870cc3bf31bb37ba4f1
Marc Zoeller <m.zoeller@usu.de>
2021-10-10 17:17:01 +0200

    Cleanup calculation of tree layout

--- src/components/tree_structure.tsx
+++ src/components/tree_structure.tsx
@@ -58,7 +58,7 @@ export class GraphNode<Datum> extends React.Component<GraphNodeProps<Datum>, {}>
                 update={{x: [node.x], y: [node.y], opacity: [1], r: [10], timing: {duration: 750, ease: easeExpInOut}}}
                 enter={{x: [node.x], y: [node.y], opacity: [1], r: [10], timing: {duration: 750, ease: easeExpInOut}}}
             >{({x: x, y: y, opacity: opacity, r: r}) =>
-                <g className={`hierarchical-tree_node ${className}`} transform={`translate(${y},${x})`}
+                <g className={`hierarchical-tree_node ${className}`} transform={`translate(${x},${y})`}
                    onClick={this.handleClick}>
                     <foreignObject x={0} y={-nodeHeight / 2} width={nodeWidth} height={nodeHeight}>
                         <div className={`hierarchical-tree_node-container`}>
@@ -90,16 +90,16 @@ export class GraphEdge<Datum> extends React.Component<GraphElementProps<Datum>, 
         return (
             <Animate
                 start={{
-                    source: {x: parent.x, y: parent.y + nodeWidth},
+                    source: {x: parent.x + nodeWidth, y: parent.y},
                     target: {x: parent.x, y: parent.y}
                 }}
                 update={{
-                    source: {x: [parent.x], y: [parent.y + nodeWidth]},
+                    source: {x: [parent.x + nodeWidth], y: [parent.y]},
                     target: {x: [node.x], y: [node.y]},
                     timing: {duration: 750, ease: easeExpInOut}
                 }}
                 enter={{
-                    source: {x: [parent.x], y: [parent.y + nodeWidth]},
+                    source: {x: [parent.x + nodeWidth], y: [parent.y]},
                     target: {x: [node.x], y: [node.y]},
                     timing: {duration: 750, ease: easeExpInOut}
                 }}
@@ -107,7 +107,7 @@ export class GraphEdge<Datum> extends React.Component<GraphElementProps<Datum>, 
                 <path
                     className={`hierarchical-tree_link ${className}`}
                     d={
-                        d3.linkHorizontal().x(d => d[1]).y(d => d[0])({
+                        d3.linkHorizontal().x(d => d[0]).y(d => d[1])({
                             source: [source.x, source.y],
                             target: [target.x, target.y]
                         })}/>
@@ -126,32 +126,51 @@ interface HierarchicalTreeProps<Datum> {
     render: (node: CollapsiblePointNode<Datum>) => JSX.Element
 }
 
-export class HierarchicalTree<Datum> extends React.Component<HierarchicalTreeProps<Datum>, {}> {
+interface HierarchicalTreeState {
+    containerWidth: number
+    containerHeight: number
+}
+
+export class HierarchicalTree<Datum> extends React.Component<HierarchicalTreeProps<Datum>, HierarchicalTreeState> {
     private readonly containerRef: React.RefObject<SVGSVGElement> = React.createRef<SVGSVGElement>();
     private readonly layout: TreeLayout<Datum> = d3.tree<Datum>().size([1, 1]);
-    private readonly margin: number = 20;
+    private readonly margin: number = 10;
+
+    constructor(props: HierarchicalTreeProps<Datum>) {
+        super(props);
+        this.state = {containerWidth: 0, containerHeight: 0}
+    }
+
+    componentDidMount() {
+        this.setState({
+            containerWidth: this.containerRef.current?.clientWidth,
+            containerHeight: this.containerRef.current?.clientHeight
+        });
+    }
 
     private calcHeight(root: CollapsibleNode<Datum>): number {
-        if (!root) {
-            return 50
-        }
-
         const {nodeWidth, nodeHeight} = this.props
 
         const nodeCount = new Array<number>(Math.max(...root.descendants().map(d => d.depth)) + 1).fill(0);
         root.descendants().map(d => nodeCount[d.depth]++);
         const maxNodes = Math.max(...nodeCount);
-        const newHeight = maxNodes * (1.1 * nodeHeight + this.margin) + 2 * this.margin;
-
-        (root.descendants() as CollapsiblePointNode<Datum>[])
-            .map(d => {
-                d.y = d.depth * (nodeWidth + 75);
-                d.x = d.x * (newHeight - 2 * this.margin);
-                return d;
-            })
+        const newHeight = 1.3 * maxNodes * nodeHeight;
 
         if (this.containerRef.current) {
             const currentHeight = this.containerRef.current.clientHeight
+            const currentWidth = this.containerRef.current.clientWidth;
+
+            (root.descendants() as CollapsiblePointNode<Datum>[])
+                .map(d => {
+                    // Flip x and y to get horizontal layout
+                    const x = d.x
+                    const y = d.y
+
+                    d.y = x * newHeight;
+                    d.x = y * (currentWidth - nodeWidth - 2*this.margin);
+                    return d;
+                })
+
             if (currentHeight > newHeight) {
                 window.setTimeout(() => {
                     this.containerRef.current.setAttribute('height', newHeight.toString());
@@ -174,7 +193,7 @@ export class HierarchicalTree<Datum> extends React.Component<HierarchicalTreePro
 
         return (
             <svg className={'hierarchical-tree'} ref={this.containerRef} height={height}>
-                {root && <g transform={`translate(${this.margin},${this.margin})`}>
+                {root && <g transform={`translate(${this.margin},0)`}>
                     {this.props.render(root)}
                 </g>}
             </svg>
