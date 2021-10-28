860c4ca0dfd5d6182f311fc1562dd04e9c7dc8a0
Marc Zoeller <m.zoeller@usu.de>
2021-10-28 12:03:58 +0200

    Improve design of component tooltip

--- src/components/structure_graph.tsx
+++ src/components/structure_graph.tsx
@@ -4,10 +4,126 @@ import * as dagre from "dagre";
 import {graphlib} from "dagre";
 import {fixedPrec, normalizeComponent} from "../util";
 import {Table, TableBody, TableCell, TableRow, Tooltip, Typography} from "@material-ui/core";
-import {OutputDescriptionData, requestOutputDescription, ServerError} from "../handler";
+import {OutputDescriptionData, requestOutputDescription} from "../handler";
 import {LoadingIndicator} from "./loading";
 import {ErrorIndicator} from "../util/error";
 
+
+interface Node {
+    label: string
+    config: Config
+    x: number,
+    y: number,
+    width: number
+    height: number
+}
+
+interface SingleComponentProps {
+    id: string
+    node: Node
+
+    error: Error
+    loading: boolean
+    output: string
+
+    onHover: () => void
+    onClick?: (component: [string, string]) => void
+}
+
+class SingleComponent extends React.Component<SingleComponentProps, any> {
+
+    render() {
+        const {id, node, error, loading, output, onHover, onClick} = this.props
+
+        const configTable: [[string, ConfigValue][], [string, ConfigValue][]] = [[], []]
+        Array.from(node.config?.entries())
+            .forEach(([name, value], idx) => {
+                configTable[idx % 2].push([name, value])
+            })
+        // Ensure that left and right array have exactly the same amount of elements
+        if (configTable[0].length != configTable[1].length)
+            configTable[1].push(["", ""])
+
+        const configuration = <>
+            <Typography color="inherit" component={'h4'}>Configuration</Typography>
+            <Table>
+                <TableBody>
+                    {configTable[0]
+                        .map(([name, value], idx) => {
+                            const name2 = configTable[1][idx][0]
+                            const value2 = configTable[1][idx][1]
+
+                            return (
+                                <TableRow key={name}>
+                                    <TableCell component="th"
+                                               scope="row">{name}</TableCell>
+                                    <TableCell align="right">{
+                                        typeof value === 'number' ? fixedPrec(value, 5) : String(value)
+                                    }</TableCell>
+
+                                    <TableCell component="th"
+                                               scope="row">{name2}</TableCell>
+                                    <TableCell align="right">{
+                                        typeof value2 === 'number' ? fixedPrec(value2, 5) : String(value2)
+                                    }</TableCell>
+                                </TableRow>
+                            )
+                        })
+                    }
+                </TableBody>
+            </Table>
+        </>
+
+        const tooltipContent = <>
+            {node.config.size > 0 ?
+                configuration :
+                <Typography color="inherit" component={'h4'}>No Configuration</Typography>}
+            <hr/>
+            <Typography color="inherit" component={'h4'}>Output</Typography>
+
+            <ErrorIndicator error={error}/>
+            {!error &&
+            <>
+                <LoadingIndicator loading={loading}/>
+                {!loading && (output ?
+                    <div style={{overflowX: "auto", marginBottom: 0}}
+                         dangerouslySetInnerHTML={{__html: output}}/> : <div>Missing</div>)
+                }
+            </>}
+        </>
+
+        return (
+            <g key={id}
+               transform={`translate(${node.x - node.width / 2}, ${node.y - node.height / 2})`}>
+                <foreignObject
+                    width={`${node.width}px`}
+                    height={`${node.height}px`}
+                    requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility"
+                    onClick={(e) => {
+                        if (!!onClick) {
+                            onClick([id, node.label])
+                            e.stopPropagation()
+                        }
+                    }}>
+                    <Tooltip placement={'top'}
+                             classes={{tooltip: 'structure-graph_tooltip jp-RenderedHTMLCommon'}}
+                             title={tooltipContent}
+                             enterDelay={500}
+                             enterNextDelay={500}
+                             leaveDelay={500}
+                             interactive={true}
+                             onOpen={onHover}>
+                        {isPipEnd(id) ?
+                            <div className={'structure-graph_node structure-graph_end-node'}/> :
+                            <div className={'structure-graph_node'}>{node.label}</div>
+                        }
+                    </Tooltip>
+                </foreignObject>
+            </g>
+
+        )
+    }
+}
 
 interface StructureGraphProps {
     structure: Structure
@@ -35,14 +151,12 @@ export class StructureGraphComponent extends React.Component<StructureGraphProps
     }
 
     fetchOutputs() {
-        if (this.state.loading) {
+        if (this.state.loading)
             // Loading already in progress
             return
-        }
-        if (this.state.outputs.size > 0) {
+        if (this.state.outputs.size > 0)
             // Outputs already cached
             return
-        }
 
         this.setState({loading: true})
         requestOutputDescription(this.props.candidate.id, this.props.meta.data_file, this.props.meta.model_dir)
@@ -53,25 +167,19 @@ export class StructureGraphComponent extends React.Component<StructureGraphProps
             });
     }
 
-    private static isPipEnd(id: string): boolean {
-        return id === StructureGraphComponent.SOURCE || id === StructureGraphComponent.SINK
-    }
-
     render() {
-        const {structure, candidate} = this.props
+        const {structure, candidate, onComponentSelection} = this.props
         const {outputs, loading, error} = this.state
 
         const nodeDimensions = {width: 100, height: 40}
         const margin = {top: 5, left: 10}
 
-        const graph = new graphlib.Graph();
+        const graph = new graphlib.Graph<Node>();
         graph.setGraph({rankdir: 'LR', nodesep: 20, ranksep: 20});
         graph.setDefaultEdgeLabel(() => ({}))
 
         const edges: [string, string][] = []
         const nodeMap = new Map<string, string>([[StructureGraphComponent.SOURCE, 'Source'], [StructureGraphComponent.SINK, 'Sink']])
-
-
         let source = StructureGraphComponent.SOURCE
         structure.pipeline.steps.forEach(([id, label]) => {
             nodeMap.set(id, label)
@@ -79,95 +187,38 @@ export class StructureGraphComponent extends React.Component<StructureGraphProps
             source = id
         })
         edges.push([source, StructureGraphComponent.SINK])
-        nodeMap.forEach((label, id) => graph.setNode(id, {
-            label: normalizeComponent(label),
-            width: StructureGraphComponent.isPipEnd(id) ? nodeDimensions.width / 4 : nodeDimensions.width,
-            height: StructureGraphComponent.isPipEnd(id) ? nodeDimensions.width / 4 : nodeDimensions.height
-        }))
+
+        nodeMap.forEach((label, id) => {
+            const prefix = `${id}:`
+            const subConfig = new Map<string, ConfigValue>()
+
+            Array.from(candidate.config.keys())
+                .filter(k => k.startsWith(prefix))
+                .forEach(key => {
+                    subConfig.set(key.substring(prefix.length), candidate.config.get(key))
+                })
+
+            graph.setNode(id, {
+                label: normalizeComponent(label),
+                config: subConfig,
+                width: isPipEnd(id) ? nodeDimensions.width / 4 : nodeDimensions.width,
+                height: isPipEnd(id) ? nodeDimensions.width / 4 : nodeDimensions.height
+            })
+        })
         edges.forEach(([source, target]) => graph.setEdge(source, target))
         dagre.layout(graph);
-
-        const subConfigs = new Map<string, Config>(
-            structure.pipeline.steps
-                .map(([id, _]) => id)
-                .map(step => {
-                        const prefix = `${step}:`
-                        const subConfig = new Map<string, ConfigValue>()
-
-                        Array.from(candidate.config.keys())
-                            .filter(k => k.startsWith(prefix))
-                            .forEach(key => {
-                                subConfig.set(key.substring(prefix.length), candidate.config.get(key))
-                            })
-                        return [step, subConfig]
-                    }
-                )
-        )
 
         return (
             <svg style={{width: '100%', height: '100%'}}>
                 <g id={"transformGroup"} transform={`translate(${margin.left},${margin.top})`}>
                     {graph.nodes().map(id => {
-                        const node = graph.node(id)
-                        const content = StructureGraphComponent.isPipEnd(id) ?
-                            <div className={'structure-graph_node structure-graph_end-node'}/> :
-                            <div className={'structure-graph_node'}>{node.label}</div>
-
-                        const configuration = subConfigs.has(id) && subConfigs.get(id).size > 0 ? <>
-                            <Typography color="inherit" component={'h4'}>Configuration</Typography>
-                            <Table>
-                                <TableBody>
-                                    {Array.from(subConfigs.get(id)?.entries())
-                                        .map(([name, value]) => (
-                                            <TableRow key={name}>
-                                                <TableCell component="th"
-                                                           scope="row">{name}</TableCell>
-                                                <TableCell align="right">{
-                                                    typeof value === 'number' ? fixedPrec(value, 5) : String(value)
-                                                }</TableCell>
-                                            </TableRow>
-                                        ))}
-                                </TableBody>
-                            </Table>
-                        </> : <Typography color="inherit" component={'h4'}>No Configuration</Typography>
-
-                        const output = outputs.has(id) ?
-                            <div dangerouslySetInnerHTML={{__html: outputs.get(id)}}/> : <div>Missing</div>
-
-                        const tooltipContent = <>
-                            {configuration}
-                            <hr/>
-                            <Typography color="inherit" component={'h4'}>Output</Typography>
-
-                            <ErrorIndicator error={error}/>
-                            {!error &&
-                            <>
-                                <LoadingIndicator loading={loading}/>
-                                {!loading && output}
-                            </>}
-                        </>
-
-                        return <g key={id}
-                                  transform={`translate(${node.x - node.width / 2}, ${node.y - node.height / 2})`}>
-                            <foreignObject
-                                width={`${node.width}px`}
-                                height={`${node.height}px`}
-                                requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility"
-                                onClick={(e) => {
-                                    if (!!this.props.onComponentSelection) {
-                                        this.props.onComponentSelection([id, node.label])
-                                        e.stopPropagation()
-                                    }
-                                }}>
-                                <Tooltip placement={'top'}
-                                         classes={{tooltip: 'structure-graph_tooltip jp-RenderedHTMLCommon'}}
-                                         title={tooltipContent}
-                                         enterDelay={750}
-                                         onOpen={this.fetchOutputs}>
-                                    {content}
-                                </Tooltip>
-                            </foreignObject>
-                        </g>
+                        return <SingleComponent id={id}
+                                                node={graph.node(id)}
+                                                loading={loading}
+                                                error={error}
+                                                output={outputs.get(id)}
+                                                onHover={this.fetchOutputs}
+                                                onClick={onComponentSelection}/>
                     })}
 
                     {graph.edges().map(edge => {
@@ -183,11 +234,13 @@ export class StructureGraphComponent extends React.Component<StructureGraphProps
                             />
                         );
                     })}
-
-
                 </g>
             </svg>
         )
     }
 
+}
+
+function isPipEnd(id: string): boolean {
+    return id === StructureGraphComponent.SOURCE || id === StructureGraphComponent.SINK
 }
