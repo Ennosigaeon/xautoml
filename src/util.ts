1109685c19a4cc3cdc3dd0e0dad7cfe5a8d8b2bb
Marc Zoeller <m.zoeller@usu.de>
2021-12-12 17:40:27 +0100

    Remove broken catchReactWarnings

--- src/util.ts
+++ src/util.ts
@@ -7,19 +7,6 @@ export type Primitive = string | number | boolean | Date | undefined
 // For reasons, JupyterContext can not be declared in root.tsx and imported in dataset_details.tsx...
 export const JupyterContext = React.createContext<Jupyter>(undefined)
 
-export function catchReactWarnings() {
-    // @ts-ignore
-    console.oldError = console.error;
-    console.error = (...args: any[]) => {
-        const ignore = args.filter(e => typeof e == 'string')
-            // DataGrid keeps on complaining that only 1 element can be selected programmatically in the MIT version. This is not true, therefore we simply ignore the warning
-            .filter(m => m.indexOf('can only be of 1 item in DataGrid') != -1);
-        if (ignore.length === 0) { // @ts-ignore
-            return console.oldError(...args);
-        }
-    }
-}
-
 export function cidToSid(cid: CandidateId): string {
     return cid.substring(0, cid.indexOf(':', 4))
 }
