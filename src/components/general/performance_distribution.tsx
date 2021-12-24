2ab7fb11669305606afc3b9077932394502077a8
Marc Zoeller <m.zoeller@usu.de>
2021-12-24 10:41:46 +0100

    Add performance distribution plot

---
+++ src/components/general/performance_distribution.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {MetaInformation} from "../../model";
+import {Bar, BarChart, CartesianGrid, ResponsiveContainer, XAxis, YAxis} from "recharts";
+import {LoadingIndicator} from "../../util/loading";
+import {TimelineRecord} from "./performance_timeline";
+import {bin} from "d3";
+
+interface PerformanceDistributionProps {
+    data: TimelineRecord[]
+    meta: MetaInformation
+    height: number
+}
+
+export default class PerformanceDistribution extends React.Component<PerformanceDistributionProps> {
+
+    render() {
+        const {data} = this.props
+
+        const bins = bin()(data.map(d => d.performance as number))
+            .map(bin => ({frequency: bin.length, performance: (bin.x0 + bin.x1) / 2}))
+
+        console.log(bins)
+
+        return (
+            <div style={{height: this.props.height}}>
+                <LoadingIndicator loading={data.length === 0}/>
+
+                {data.length > 0 &&
+                    <ResponsiveContainer>
+                        <BarChart data={bins}>
+                            <CartesianGrid strokeDasharray="3 3"/>
+
+                            <XAxis dataKey="performance" label={{value: this.props.meta.metric, dy: 10}}/>
+                            <YAxis label={{value: 'Frequency', angle: -90, dx: -25}}/>
+
+                            <Bar dataKey="frequency" fill="#82ca9d"/>
+                        </BarChart>
+                    </ResponsiveContainer>
+                }
+            </div>
+        );
+    }
+}
--- src/components/general_information.tsx
+++ src/components/general_information.tsx
@@ -4,6 +4,10 @@ import PerformanceTimeline, {TimelineRecord} from "./general/performance_timelin
 import {RocCurve} from "./general/roc_curve";
 import React from "react";
 import {Candidate, CandidateId, MetaInformation, Structure} from "../model";
+import {TabContext, TabPanel} from "@material-ui/lab";
+import {Box, Tab, Tabs} from "@material-ui/core";
+import {Colors} from "../util";
+import PerformanceDistribution from "./general/performance_distribution";
 
 
 interface GeneralInformationProps {
@@ -16,13 +20,16 @@ interface GeneralInformationProps {
 
 interface GeneralInformationState {
     timeline: TimelineRecord[]
+    openTab: string
 }
 
 export class GeneralInformation extends React.Component<GeneralInformationProps, GeneralInformationState> {
 
     constructor(props: GeneralInformationProps) {
         super(props)
-        this.state = {timeline: []}
+        this.state = {timeline: [], openTab: '1'}
+
+        this.switchTab = this.switchTab.bind(this)
     }
 
     componentDidMount() {
@@ -33,18 +40,38 @@ export class GeneralInformation extends React.Component<GeneralInformationProps,
         this.setState({timeline: runTimeline})
     }
 
+    private switchTab(_: any, selectedTab: string) {
+        this.setState({openTab: selectedTab})
+    }
 
     render() {
         const {meta, candidateMap, selectedCandidates, onCandidateSelection} = this.props
+        const {openTab} = this.state
 
         return (
             <>
                 <MetaInformationTable meta={meta}/>
                 <CollapseComp showInitial={true} help={PerformanceTimeline.HELP}>
-                    <h4>Performance Timeline</h4>
-                    <PerformanceTimeline data={this.state.timeline} meta={meta} height={250}
-                                         selectedCandidates={selectedCandidates}
-                                         onCandidateSelection={onCandidateSelection}/>
+                    <h4>Performance Overview</h4>
+                    <TabContext value={openTab}>
+                        <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+                            <Tabs value={openTab} onChange={this.switchTab} style={{minHeight: 0}} TabIndicatorProps={{
+                                style: {backgroundColor: Colors.HIGHLIGHT}
+                            }}>
+                                <Tab className={'tab-small'} label="Timeline" value={'1'}/>
+                                <Tab className={'tab-small'} label="Distribution" value={'2'}/>
+                            </Tabs>
+                        </Box>
+
+                        <TabPanel value={'1'}>
+                            <PerformanceTimeline data={this.state.timeline} meta={meta} height={220}
+                                                 selectedCandidates={selectedCandidates}
+                                                 onCandidateSelection={onCandidateSelection}/>
+                        </TabPanel>
+                        <TabPanel value={'2'} style={{padding: 0, paddingTop: "5px"}}>
+                            <PerformanceDistribution data={this.state.timeline} meta={meta} height={220}/>
+                        </TabPanel>
+                    </TabContext>
                 </CollapseComp>
                 <CollapseComp showInitial={true} help={RocCurve.HELP}>
                     <h4>ROC Curve</h4>
