07930e746fc0b5103de249eb47172e6ca4bcf198
Marc Zoeller <m.zoeller@usu.de>
2022-01-05 02:01:22 +0100

    Use stroke data set instead of hearts

--- xautoml/util/datasets.py
+++ xautoml/util/datasets.py
@@ -51,6 +51,51 @@ def hearts(file: str, train: bool = False, test: bool = False):
     return X.reset_index(drop=True), y.reset_index(drop=True)
 
 
+def stroke(file: str, train: bool = False, test: bool = False):
+    if test == train:
+        raise ValueError('Please set either train or test to True')
+
+    data = pd.read_csv(file)
+
+    data.loc[:, 'gender'] = data.gender.astype('category')
+    data.loc[:, 'ever_married'] = data.ever_married.astype('category')
+    data.loc[:, 'work_type'] = data.work_type.astype('category')
+    data.loc[:, 'Residence_type'] = data.Residence_type.astype('category')
+    data.loc[:, 'smoking_status'] = data.smoking_status.astype('category')
+
+    data.drop(columns=['age'], inplace=True)
+
+    X = data.loc[:, data.columns[:-1]]
+    y = data.loc[:, data.columns[-1]]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, stratify=y)
+
+    if train:
+        try:
+            from imblearn.over_sampling import SMOTENC
+            X_train.bmi[pd.isna(X_train.bmi)] = -1
+
+            smote = SMOTENC([1, 4, 5, 6, 9], random_state=1)
+            X, y = smote.fit_resample(X_train, y_train)
+
+            X.bmi[X.bmi < 11.5] = np.nan
+        except ImportError:
+            from sklearn.utils import resample
+            X_train['stroke'] = y_train
+            df_majority = X_train[X_train.stroke == 0]
+            df_minority = X_train[X_train.stroke == 1]
+
+            df_minority_upsampled = resample(df_minority, replace=True, n_samples=400, random_state=1)
+            df = pd.concat([df_majority, df_minority_upsampled])
+            X = df.loc[:, df.columns[:-1]]
+            y = df.loc[:, df.columns[-1]]
+    else:
+        X = X_test
+        y = y_test
+
+    return X.reset_index(drop=True), y.reset_index(drop=True)
+
+
 def down_sample(X: pd.DataFrame, y: pd.Series, n_samples: int) -> tuple[pd.DataFrame, pd.Series]:
     if X.shape[0] > n_samples:
         idx = np.random.choice(X.shape[0], size=n_samples, replace=False)
